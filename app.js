415aa573e4a9f49a5388d26d5db94d6f3b2a1f55
dhruv jain <dhruvjain78791999@gmail.com>
2025-08-02 17:06:25 +0530

    demo

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express();
+const bodyParser = require('body-parser');
+const { GoogleGenerativeAI } = require('@google/generative-ai');
+//require('dotenv').config();
+
+app.use(bodyParser.json());
+
+app.post('/getResponse', (req, res) => {
+    console.log(req.body.question);
+    const genAI = new GoogleGenerativeAI('AIzaSyCBCu8a0QP4DZEByM3SYZbAxm6AzMnaNf8')
+    const model =  genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
+    model.generateContent(req.body.question).then(result => {
+        console.log(result.response.text());
+        const response = result.response.text()
+        res.status(200).json({
+            response: response
+        })
+    }).catch(err=>{
+        console.log(err , "error hai bhai");
+        res.status(500).json({
+            error:err
+        })
+    })
+
+
+})
+
+
+
+module.exports = app
